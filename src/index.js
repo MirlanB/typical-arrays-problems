40a49a7dad2e2f56f29bb284c4be3997c4e94f2e
MirlanB <78996635+MirlanB@users.noreply.github.com>
2021-03-18 19:16:14 +0300

    fix: add

--- src/index.js
+++ src/index.js
@@ -24,3 +24,24 @@ exports.avg = function avg (array) {
     let avg = total / array.length;
     return avg;
 }
+
+
+// exports.min = function min (array) {
+//   if (!array || array.length === 0) return 0;
+//   array.sort((a, b) => a - b);
+//   return array[0];
+// }
+
+// exports.max = function max (array) {
+//   if (!array || array.length === 0) return 0;
+//   array.sort((a, b) => b - a);
+//   return array[0];
+// }
+
+// exports.avg = function avg (array) {
+//   if (!array || array.length === 0) return 0;
+//   let len = array.length;
+//   let sum = array.reduce((a, b) => a + b);
+//   return sum / len;
+// }
+
